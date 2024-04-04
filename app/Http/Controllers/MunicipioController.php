837187fef9968c62604ca8775c393568b5a75f6d
giovannisaavedra160@gmail.com <giovannisaavedra160@gmail.com>
2024-04-04 09:10:28 -0500

    municipio Add

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -27,7 +27,11 @@ class MunicipioController extends Controller
      */
     public function create()
     {
-        //
+        $departamentos = DB::table('tb_departamento') 
+          ->orderBy('depa_nomb')
+           ->get();
+
+        return view('municipio.new', ['departamentos' => $departamentos]);
     }
 
     /**
@@ -35,7 +39,13 @@ class MunicipioController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $municipios = DB::table('tb_municipio')
+            ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+            ->select('tb_municipio.*', 'tb_departamento.depa_nomb')
+            ->orderBy('tb_municipio.muni_nomb')
+            ->get();
+        
+        return view('municipio.index', ['municipios' => $municipios]);
     }
 
     /**
