fbf3c06119966b718c683830f0701a6ff144ecc2
giovannisaavedra160@gmail.com <giovannisaavedra160@gmail.com>
2024-04-04 08:49:01 -0500

    municipio

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\MunicipioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,3 +14,5 @@ Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comuna
 Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
 Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
 Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+
+Route::get('/municipios', [MunicipioController::class, 'index'])->name('municipios.index');
